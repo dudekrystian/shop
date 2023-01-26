ed700a9e50d777aee5c6d71b5228bafcd5f668b6
dudekrystian <dudekrystian@gmail.com>
2023-01-26 22:17:00 +0100

    add comptonens to main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import "../src/scss/style.css";
+
 import Header from "./components/header/Header";
+import Category from "./components/category/Category";
+import Sale from "./components/sale/Sale";
+import ListOfProducts from "./components/products/ListOfProducts";
+
+//pages
+
+import SingleProduct from "./pages/singleProduct";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <Category />
+      {/* <Sale />
+      <Sale />
+      <Sale /> */}
+      {/* <ListOfProducts /> */}
+      <SingleProduct />
+      <Footer />
     </div>
   );
 }
