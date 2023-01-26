38119f2454dfbbc5d1937dfcc26d73d09fb49315
dudekrystian <dudekrystian@gmail.com>
2023-01-26 22:12:14 +0100

    create examples of categories card

---
+++ src/components/category/CardCategory.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function CardCategory() {
+  return (
+    <>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="single">
+          <i class="bi bi-laptop"></i>
+          <span>Laptopy i Komputery</span>
+        </div>
+      </div>
+    </>
+  );
+}
