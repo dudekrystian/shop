7818f116338ec2e03ef501827ac2af2073cc9fc0
dudekrystian <dudekrystian@gmail.com>
2023-01-26 22:09:55 +0100

    create components with scroll categories menu

---
+++ src/components/category/Category.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CardCategory from "./CardCategory";
+
+export default function Category() {
+  return (
+    <div className="category">
+      <CardCategory />
+    </div>
+  );
+}
