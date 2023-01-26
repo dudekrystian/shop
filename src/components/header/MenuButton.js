7da266c1a0d79ce83f50632d26243b72eb5d4341
dudekrystian <dudekrystian@gmail.com>
2023-01-26 22:09:22 +0100

    hamburger menu

---
+++ src/components/header/MenuButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MenuButton() {
+  return (
+    <div className="menu">
+      <div className="element">
+        <i class="bi bi-list"></i>
+        <span>Menu</span>
+      </div>
+    </div>
+  );
+}
