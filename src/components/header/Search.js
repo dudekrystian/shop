127b6b5e78c9f0aac29562614e47e0eaab409b90
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:52:47 +0100

    add functions to comptonent

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -1,9 +1,27 @@
-import React from "react";
+import Reac, { useEffect, useState } from "react";
+import { setSearchTerm } from "../../store/searchSlice";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setSidebarOn } from "../../store/sidebarSlice";
+import { getAllCategories } from "../../store/categorySlice";
 
 export default function Search() {
+  const dispatch = useDispatch();
+  const categories = useSelector(getAllCategories);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  };
   return (
     <div className="search">
-      <input type="search" placeholder="Czego potrzebujesz?"></input>
+      <input
+        onChange={(e) => handleSearchTerm(e)}
+        type="search"
+        placeholder="What you need?"
+      ></input>
+      <Link to={`search/${searchTerm}`}></Link>
     </div>
   );
 }
