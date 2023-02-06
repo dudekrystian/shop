df18dc6e370968c539e8ab46d3435d8d81e23714
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:47:49 +0100

    create componet

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div className="loader">
+      <span className="dot"></span>
+      <div className="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
