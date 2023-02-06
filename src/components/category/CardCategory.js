b27018a0166cdfaf99167cca4dfcaa5731c1ecc1
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:59:12 +0100

    rebuild comptonent: add link to target category

--- src/components/category/CardCategory.js
+++ src/components/category/CardCategory.js
@@ -1,54 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CardCategory() {
+export default function CardCategory({ name }) {
   return (
     <>
       <div className="card">
         <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="single">
-          <i class="bi bi-laptop"></i>
-          <span>Laptopy i Komputery</span>
+          <i class="bi bi-bag"></i>
+          <Link className="link" to={`category/${name}`}>
+            {name}
+          </Link>
         </div>
       </div>
     </>
