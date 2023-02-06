c1155db5846c1ad458646dd1ef65dc3d6dde4569
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:58:22 +0100

    add data from Cart; rebuild counter

--- src/components/header/UserPanel.js
+++ src/components/header/UserPanel.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getAllCarts,
+  getCartItemsCount,
+  getCartTotal,
+} from "../../store/cartSlice";
 
 export default function UserPanel() {
+  const dispatch = useDispatch();
+  const carts = useSelector(getAllCarts);
+  const itemsCount = useSelector(getCartItemsCount);
+
+  useEffect(() => {
+    dispatch(getCartTotal());
+  }, [carts]);
+
+  const visible = {
+    visibility: "visible",
+  };
+
+  const hidden = {
+    visibility: "hidden",
+  };
+
   return (
     <div className="user-panel">
       <div className="user-panel element">
@@ -11,10 +34,17 @@ export default function UserPanel() {
         <i class="bi bi-heart"></i>
         <span>Your favourite </span>
       </div>
-      <div className="user-panel element">
-        <i class="bi bi-cart2"></i>
-        <span>Your order </span>
-      </div>
+      <Link className="link" to="/cart">
+        <div className="user-panel element">
+          <i class="bi bi-cart2"></i>
+
+          <div className="container-cart" style={itemsCount ? visible : hidden}>
+            <span className="counter">{itemsCount} </span>
+          </div>
+
+          <span>Your order</span>
+        </div>
+      </Link>
     </div>
   );
 }
