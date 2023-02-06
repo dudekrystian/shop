67ffea1da25d2930f86f16e0694a235424a55f65
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:44:49 +0100

    add store Provider and routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "../src/scss/style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Category from "./components/category/Category";
@@ -7,20 +8,39 @@ import ListOfProducts from "./components/products/ListOfProducts";
 
 //pages
 
-import SingleProduct from "./pages/singleProduct";
+import SingleProduct from "./pages/SingleProduct";
 import Footer from "./components/footer/Footer";
+import Sidebar from "./components/header/Sidebar";
+import Home from "./pages/Home";
+import SingleCategories from "./pages/SingleCategories";
+
+import store from "./store/store";
+import { Provider } from "react-redux";
+import Search from "./pages/Search";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Category />
-      {/* <Sale />
+      <Provider store={store}>
+        <BrowserRouter>
+          <Sidebar />
+          <Header />
+          <Category />
+          {/* <Sale />
       <Sale />
       <Sale /> */}
-      {/* <ListOfProducts /> */}
-      <SingleProduct />
-      <Footer />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<SingleProduct />} />
+            <Route path="/category/:category" element={<SingleCategories />} />
+            <Route path="/search/:searchTerm" element={<Search />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
