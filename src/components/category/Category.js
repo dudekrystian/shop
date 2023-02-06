2ba8ece27aaec10b586ad0e974f910a19a154b9f
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:59:44 +0100

    add data from store

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,10 +1,18 @@
-import React from "react";
 import CardCategory from "./CardCategory";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setSidebarOn } from "../../store/sidebarSlice";
+import { getAllCategories } from "../../store/categorySlice";
 
 export default function Category() {
+  const dispatch = useDispatch();
+  const categories = useSelector(getAllCategories);
+
   return (
     <div className="category">
-      <CardCategory />
+      {categories.slice(0, 11).map((category, idx) => (
+        <CardCategory name={category} key={idx} />
+      ))}
     </div>
   );
 }
