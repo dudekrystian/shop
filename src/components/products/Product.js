03e173bcac54e95023f803134bb65a5d7ad3ac51
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:56:09 +0100

    rebuild comptonent: add date

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,35 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { currencyFormatter } from "../../utils/formatCurrency";
 
-export default function ListProducts() {
+export default function Product({ product }) {
   return (
-    <div className="product-container">
-      <div className="product-image">
-        <img
-          src="https://i.dummyjson.com/data/products/8/thumbnail.jpg"
-          alt="foto"
-        ></img>
-      </div>
-      <div className="product-name">
-        <p>Samsung Galaxy M51 LTE 5G 64RAM</p>
-        <p>Samsung</p>
-      </div>
-      <div className="rating">
+    <Link className="link" to={`/product/${product?.id}`} key={product?.id}>
+      <div className="product-container">
+        <div className="product-image">
+          <img src={product?.thumbnail} alt="foto"></img>
+        </div>
+        <div className="product-name">
+          <p>
+            {product?.title} ({product?.rating})
+          </p>
+          <span>{product?.brand}</span>
+        </div>
+        {/* <div className="rating">
         <i class="bi bi-star-fill"></i>
-      </div>
-      <div className="bottom">
-        <div className="price">
-          <span className="price-old">1699.99</span>
-          <span className="price">1299.99</span>
+      </div> */}
+        <div className="bottom">
+          <div className="price">
+            <span className="price-old">
+              {currencyFormatter.format(product?.price)}{" "}
+            </span>
+            <span className="price">
+              {currencyFormatter.format(product?.discountedPrice)}{" "}
+            </span>
+          </div>
+          <div className="options">
+            <button>
+              <i class="bi bi-heart-fill"></i>
+            </button>
+            <button>
+              <i class="bi bi-cart-plus"></i>
+            </button>
+          </div>
         </div>
-        <div className="options">
-          <button>
-            <i class="bi bi-heart-fill"></i>
-          </button>
-          <button>
-            <i class="bi bi-cart-plus"></i>
-          </button>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 }
