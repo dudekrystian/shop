d16c3d6d1a2b259d145b1b44a85b576e3770b069
dudekrystian <dudekrystian@gmail.com>
2023-02-06 21:43:48 +0100

    add main store file with imports slices

---
+++ src/store/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./sidebarSlice";
+import categoryReducer from "./categorySlice";
+import productReducer from "./productSlice";
+import searchReducer from "./searchSlice";
+import cartReducer from "./cartSlice";
+
+const store = configureStore({
+  reducer: {
+    sidebar: sidebarReducer,
+    category: categoryReducer,
+    product: productReducer,
+    cart: cartReducer,
+    search: searchReducer,
+  },
+});
+
+export default store;
