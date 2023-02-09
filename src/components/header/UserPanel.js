7b90261dd91c671dabc99c9b65705f0909849374
dudekrystian <dudekrystian@gmail.com>
2023-02-09 18:28:21 +0100

    fix class name and add correct name

--- src/components/header/UserPanel.js
+++ src/components/header/UserPanel.js
@@ -26,16 +26,16 @@ export default function UserPanel() {
 
   return (
     <div className="user-panel">
-      <div className="user-panel element">
+      <div className="user-panel-element">
         <i class="bi bi-person"></i>
         <span>Your account </span>
       </div>
-      <div className="user-panel element">
+      <div className="user-panel-element">
         <i class="bi bi-heart"></i>
         <span>Your favourite </span>
       </div>
       <Link className="link" to="/cart">
-        <div className="user-panel element">
+        <div className="user-panel-element">
           <i class="bi bi-cart2"></i>
 
           <div className="container-cart" style={itemsCount ? visible : hidden}>
